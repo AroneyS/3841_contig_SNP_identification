beff93890631ab8473ab1018ad0ea0a59b3baf92
Samuel Aroney <samuel.aroney@outlook.com>
2020-05-18 21:12:25 +0100

    automate genome components for samtools

--- code/sam2.sh
+++ code/sam2.sh
@@ -8,10 +8,7 @@ do
     samtools sort $f -o /output/${NAME}.sorted
 done
 
-samtools faidx /NCBI_3841/3841.fasta -o /NCBI_3841/3841.fasta.fai
-samtools faidx /NCBI_3841/prl7.fasta -o /NCBI_3841/prl7.fasta.fai
-samtools faidx /NCBI_3841/prl8.fasta -o /NCBI_3841/prl8.fasta.fai
-samtools faidx /NCBI_3841/prl9.fasta -o /NCBI_3841/prl9.fasta.fai
-samtools faidx /NCBI_3841/prl10.fasta -o /NCBI_3841/prl10.fasta.fai
-samtools faidx /NCBI_3841/prl11.fasta -o /NCBI_3841/prl11.fasta.fai
-samtools faidx /NCBI_3841/prl12.fasta -o /NCBI_3841/prl12.fasta.fai
+for f in /NCBI_3841/*.fasta
+do
+    samtools faidx $f -o ${f}.fai
+done
