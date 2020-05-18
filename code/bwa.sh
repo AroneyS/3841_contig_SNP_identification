27f785b0d48f975fed5a529ed96afcae3cadcb24
Samuel Aroney <samuel.aroney@outlook.com>
2020-05-18 20:51:44 +0100

    automate genome components for bwa

--- code/bwa.sh
+++ code/bwa.sh
@@ -1,9 +1,6 @@
 #!/bin/bash
 
-bwa index -a is /NCBI_3841/3841.fasta
-bwa index -a is /NCBI_3841/prl7.fasta
-bwa index -a is /NCBI_3841/prl8.fasta
-bwa index -a is /NCBI_3841/prl9.fasta
-bwa index -a is /NCBI_3841/prl10.fasta
-bwa index -a is /NCBI_3841/prl11.fasta
-bwa index -a is /NCBI_3841/prl12.fasta
+for f in /NCBI_3841/*.fasta
+do
+    bwa index -a is $f
+done
